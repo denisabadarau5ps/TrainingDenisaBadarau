59aadc87133738a8dd4e2db2aaea4fa94720f8ea
danuthostiuc <danut.hostiuc@5psolutions.eu>
2021-11-16 18:37:52 +0200

    added checkout button
    {#5} { cart.php} {http://pm.5psolutions.net/public/index.php?path_info=projects/training-denisa-badarau/tasks/5} {added checkout button, but with a bug at sending email}

--- TrainingDenisaBadarau/cart.php
+++ TrainingDenisaBadarau/cart.php
@@ -1,19 +1,71 @@
 <?php
 session_start();
 
-require  'common.php';
+require 'common.php';
 require 'config.php';
 
-if(isset($_POST['remove_from_cart'])){
-    foreach($_SESSION["cart"] as $keys => $values)
-    {
-        if($values["id"] == $_GET["id"])
-        {
+if (isset($_POST['remove_from_cart'])) {
+    foreach ($_SESSION["cart"] as $keys => $values) {
+        if ($values["id"] == $_GET["id"]) {
             unset($_SESSION["cart"][$keys]);
         }
     }
-
 }
+
+if (isset($_POST['checkout'])) {
+    echo '<script>alert("Checkout!")</script>';
+    $name = $_POST['name'];
+    $contacts = $_POST['contact'];
+    $comments = $_POST['comments'];
+
+    $to = strval($shopManagerEmail);
+    $subject = "Shopping cart";
+    $message = '
+    <html>
+    <head>
+    <title>Shopping cart</title>
+    </head>
+    <body>';
+    if (!empty($_SESSION['cart'])) {
+        $product_id_array = array_column($_SESSION['cart'], "id");
+        $cart = implode(",", $product_id_array);
+        $sql = "SELECT * FROM products WHERE id IN($cart)";
+        $result = $conn->query($sql);
+        $message .= '    
+        <p>Name:<' . $name . '</p>
+        <p>Contacts:' . $contacts . '</p>
+        <p>Comments:' . $comments . '</p>
+    ';
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()):
+                $id = $row["id"];
+                $title = $row["title"];
+                $description = $row["description"];
+                $price = $row["price"];
+
+                $message .= '
+                <div class="product-container">
+                    <img class="product-image" src="images/' . $id . '.jpg" alt="Product Image" width="600" height="400">
+                    <h3>' . $title . '</h3>
+                    <div class="product-desc">
+                        ' . $description . '
+                        <br>
+                      ' . $price . ' $
+                    </div>
+                </div>';
+
+            endwhile;
+
+        }
+        unset($_SESSION['cart']);
+        // It is mandatory to set the content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        #mail($to,$subject,$message,$headers);
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -26,22 +78,22 @@ if(isset($_POST['remove_from_cart'])){
 
 <?php
 
-if(!empty($_SESSION['cart'])){
-    $product_id_array=array_column($_SESSION['cart'],"id");
-    $cart=implode(",",$product_id_array);
-    $sql="SELECT * FROM products WHERE id IN($cart)";
+if (!empty($_SESSION['cart'])) {
+    $product_id_array = array_column($_SESSION['cart'], "id");
+    $cart = implode(",", $product_id_array);
+    $sql = "SELECT * FROM products WHERE id IN($cart)";
     $result = $conn->query($sql);
 
-    if ($result->num_rows > 0){
-        while($row = $result->fetch_assoc()):
-            $id=$row["id"];
-            $title=$row["title"];
-            $description=$row["description"];
-            $price= $row["price"];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()):
+            $id = $row["id"];
+            $title = $row["title"];
+            $description = $row["description"];
+            $price = $row["price"];
             ?>
             <form method="post" action="cart.php?id=<?= $id; ?>">
                 <div class="product-container">
-                    <img class="product-image" src="images/<?= $id?>.jpg" alt="Product Image" width="600" height="400">
+                    <img class="product-image" src="images/<?= $id ?>.jpg" alt="Product Image" width="600" height="400">
                     <h3><?php echo $title ?></h3>
                     <div class="product-desc">
                         <?php echo $description ?>
@@ -54,21 +106,21 @@ if(!empty($_SESSION['cart'])){
 
         <?php endwhile;
     }
-}else{
+} else {
     ?>
-<div class="product-container">
-    <img class="product-image" src="images/emptycart.png" alt="Product Image" width="600" height="400">
-</div>
-<?php
+    <div class="product-container">
+        <img class="product-image" src="images/emptycart.png" alt="Product Image" width="600" height="400">
+    </div>
+    <?php
 }
 ?>
 
-<form method="post" action= action="cart.php?">
+<form method="post" action="cart.php">
     <div class="checkout-details-container">
-        <input type="text" id="name" size="35" value="Name" required><br><br>
+        <input type="text" name="name" size="35" value="Name" required><br><br>
         <textarea id="contact" name="contact" cols="35" required>Contact details</textarea>
         <textarea id="comments" name="comments" rows="5" cols="35">Comments</textarea>
-        <button>Checkout</button>
+        <input type="submit" name="checkout" value="Checkout">
     </div>
 </form>
 
