a5c006681474a4ec8f69be2a9af41d1ba948263e
danuthostiuc <94361041+denisabadarau5ps@users.noreply.github.com>
2021-11-22 16:20:05 +0200

    Create products.php
    {#7} { products.php} {http://pm.5psolutions.net/public/index.php?path_info=projects/training-denisa-badarau/tasks/7} {create the page for all products from database, delete products from database, logout from admin account}

---
+++ products.php
@@ -0,0 +1,70 @@
+<?php
+require_once 'common.php';
+require_once 'product.functions.php';
+
+if (!isset($_SESSION['username'])) {
+    header('location:login.php');
+    exit;
+}
+if (isset($_POST['edit'])) {
+    $_SESSION['prodIdEdit'] = $_GET['id'];
+    header('location:product.php');
+    unset($_POST['edit']);
+    exit;
+}
+if (isset($_POST['delete'])) {
+    $prodId = $_GET['id'];
+    $sql = "DELETE FROM products WHERE id=' $prodId '";
+    if ($conn->query($sql)) {
+        echo '<script>alert("Deleted")</script>';
+    }
+    unset($_SESSION['delete']);
+}
+if (isset($_GET['add'])) {
+    header('location:product.php');
+    unset($_SESSION['add']);
+    exit;
+}
+if (isset($_GET['logout'])) {
+    unset($_SESSION['username']);
+    header("Location:index.php");
+    exit;
+}
+//get all products from products table
+$data = getAllProducts();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <title><?= translate("Shopping Page") ?></title>
+</head>
+<body>
+<?php foreach ($data as $product): ?>
+    <form method="post" action="products.php?id=<?= $product->id; ?>">
+        <div class="product-container">
+            <img class="product-image" src="images/<?= $product->id ?>.jpg"
+                 alt=<?= translate("Product Image") ?>  width="600" height="400">
+            <h3><?= $product->title ?></h3>
+            <div class="product-desc">
+                <?= $product->description ?><br>
+                <?= $product->price ?> $
+            </div>
+            <input type="submit" name="edit" value="Edit">
+            <input type="submit" name="delete" value="Delete">
+        </div>
+    </form>
+<?php
+endforeach;
+$conn = null;
+?>
+<form action="products.php">
+    <div class="button-group">
+        <button name="add">Add</button>
+        <button name="logout">Logout</button>
+    </div>
+</form>
+</body>
+</html>
